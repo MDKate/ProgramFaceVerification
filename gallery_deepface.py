a55dc7306d92c73337143e36632f9b80291ee2ad
DESKTOP-MHC35ME\50AdmNsk <medngueu@gmail.com>
2023-07-28 15:21:25 +0700

    Добавлены комментарии. Внесены косметические поправки

--- gallery_deepface.py
+++ gallery_deepface.py
@@ -3,7 +3,16 @@ from deepface import DeepFace
 import pandas as pd
 import pathlib
 import os
+import wx
+import pandas as pd
+from wx.lib.pubsub import pub
+import wx.grid
+import openpyxl
+import numpy as np
+from datetime import datetime
 
+#Перечисляем возможные компоненты нейросети
+#Модель
 models = [
     "VGG-Face",  # Работает не очень
     "Facenet",  # Работает не очень
@@ -15,7 +24,9 @@ models = [
     "Dlib",  # Не работает
     "SFace",  # Удален репозиторий
 ]
+#Метрика
 metrics = ["cosine", "euclidean", "euclidean_l2"]
+#Детекция лица
 backends = [
     'opencv',
     'ssd',
@@ -25,153 +36,127 @@ backends = [
     'mediapipe'
 ]
 
-# result = DeepFace.verify(img1_path = "C:/Users/50AdmNsk/PycharmProjects/detection/train/ss/t_08.jpg",
-#                          img2_path = "C:/Users/50AdmNsk/PycharmProjects/detection/train/ss/t_18.jpg",
-#                          model_name = models[6], distance_metric = metrics[2], detector_backend = backends[1])
-
-
-# print(result.get('verified'))
-
-dfs = DeepFace.find(img_path="C:/Users/50AdmNsk/PycharmProjects/detection/train/ss/Moiseeva_Ekaterina_Dm_1.jpg",
+#Ищем скриншот в папке
+folder_path = os.path.abspath('Screen/')
+# Получаем список имен всех файлов в папке
+files = os.listdir(folder_path)
+# Фильтруем список, оставляя только файлы с расширением '.jpg'
+image_files = [file for file in files if file.endswith('.jpg')][0]
+#Компонуем нейросеть и делаем предсказание для скриншота
+dfs = DeepFace.find(img_path=os.path.abspath("Screen/"+image_files),
+# dfs = DeepFace.find(img_path="C:/Users/50AdmNsk/PycharmProjects/detection/train/ss/Moiseeva_Ekaterina_Dm_1.jpg",
                     db_path=os.path.abspath("base/"),
                     model_name=models[2], distance_metric=metrics[1], detector_backend=backends[1],
                     enforce_detection=False
                     )
-# 
-# dfs
-# 
 
 dfs = dfs[0]
 
-# dfs.to_excel('C:/Users/50AdmNsk/PycharmProjects/detection/data.txt')
-# import  wx
-#
-# app = wx.App(redirect=True)
-# top = wx.Frame(None, title="Hello World", size=(300, 200))
-# top.Show()
-# app.MainLoop()
-
-import wx
-import pandas as pd
-from wx.lib.pubsub import pub
-import wx.grid
-import openpyxl
-import numpy as np
-
+#Считываем пути к картинкам из базы
 filenames = dfs['identity'].values
+#Определяем имена из названий файлов
 name = dfs['identity'].str.split("/").str.get(-1)
-# result = np.char.split(name.values, '_')
+#Убираем лишнее из названий файлов
 result = name.str.split('_').values
 result = np.array([s[:-1] for s in result])
+#Добавляем пробелы к элементам ФИО
 result = np.char.add(result, " ")
+#Если имя и отчество
 if len(result[0]) == 2:
+    #Формируем массив из Имен и отчеств
     result = result.flatten()
     result = np.array([np.char.add(result[i], result[i+1]) for i in range(0, len(result)-1, 2)])
+#Если ФИО
 elif len(result[0]) == 3:
+    #Формируем массив из ФИО
     result = result.flatten()
     result = np.array([np.char.add(result[i], np.char.add(result[i+1], result[i+2])) for i in range(0, len(result) - 2, 3)])
-# print(result)
+#Находим самое частое ФИО
 unique_elements, counts = np.unique(result, return_counts=True)
 index = np.argmax(counts)
+#Фиксируем его как мнение нейросети
 result = unique_elements[index]
-from datetime import datetime
-
+#Запоминаем текущие дату и время
 current_date = datetime.now().date()
 current_time = datetime.now().time()
 
-# class MyFrame(wx.Frame):
-#     def __init__(self):
-#         wx.Frame.__init__(self, None, id=wx.ID_ANY, title="Resize This Window", size=wx.Size(1500, 700))
-#         panel = wx.Panel(self, -1)
-#         bSizer = wx.BoxSizer(wx.HORIZONTAL)
-#         self.SetBackgroundColour("white")
-#
-#         # Add "Identify" button
-#         self.identify_button = wx.Button(panel, label="Подтверждаю", size=(100, 20))
-#         self.identify_button.Bind(wx.EVT_BUTTON, self.on_identify)
-#         bSizer.Add(self.identify_button, 0, wx.TOP, 50)
-#         # Add "Not Recognized" button
-#         self.not_recognized_button = wx.Button(panel, label="Ошибка", size=(100, 20))
-#         self.not_recognized_button.Bind(wx.EVT_BUTTON, self.on_not_recognized)
-#         bSizer.Add(self.not_recognized_button, 0, wx.TOP, 50)
-#
-#         h = 300
-#         for i in range(len(filenames)):
-#             self.img1 = wx.Image(filenames[i], wx.BITMAP_TYPE_JPEG)
-#             self.img1 = self.img1.Scale(300, 300)
-#             self.m_bitmap1 = wx.StaticBitmap(panel, wx.ID_ANY, wx.Bitmap(self.img1))
-#             self.m_staticText1 = wx.StaticText(panel, wx.ID_ANY, name[i])
-#             bSizer.Add(self.m_bitmap1, 0, wx.TOP, 50)
-#             self.m_staticText1.SetPosition((h, 10))
-#             self.m_staticText1.SetSize((200, 30))
-#             h += 300
-#
-#         # wx.ScrolledWindow(panel, id=-1, pos=wx.DefaultPosition, size = wx.DefaultSize, style = wx.HSCROLL | wx.VSCROLL)
-#         panel.Layout()
-#         panel.Show()
-#         self.Centre(wx.BOTH)
-#         panel.SetSizerAndFit(bSizer)
+#Создаем всплывающее окно детекции
 class MyFrame(wx.Frame):
+    #Определяем всплывающее окно
     def __init__(self):
         wx.Frame.__init__(self, None, id=wx.ID_ANY, title="Resize This Window", size=wx.Size(950, 800))
         panel = wx.Panel(self, -1)
         bSizer = wx.BoxSizer(wx.VERTICAL)  # Changed to vertical sizer
+        #Фон окна
         self.SetBackgroundColour("white")
 
-        # Add "Identify" button
+        #Создаем кнопку "Подтверждаю"
         self.identifybutton = wx.Button(panel, label="Подтверждаю", size=(100, 20))
         self.identifybutton.Bind(wx.EVT_BUTTON, self.on_identify)
+        #Задаем ее положение
         bSizer.Add(self.identifybutton, 0, wx.TOP, 10)
 
-        # Add "Not Recognized" button
+        #Создаем кнопку "Ошибка"
         self.notrecognizedbutton = wx.Button(panel, label="Ошибка", size=(100, 20))
         self.notrecognizedbutton.Bind(wx.EVT_BUTTON, self.on_not_recognized)
+        #Опредедляем ее положение
         bSizer.Add(self.notrecognizedbutton, 0, wx.TOP, 10)
 
+        #Выводим фразу о мнении нейросети
         self.m_staticText1 = wx.StaticText(panel, wx.ID_ANY, "Мнение нейросети: "+result)
+        #Задаем координаты и размер фразы
         self.m_staticText1.SetPosition((110, 10))
         self.m_staticText1.SetSize((500, 30))
 
+        #Определяем ширину линий вывода изображений (4 фото в ряд)
         h = 300
         gridSizer = wx.GridSizer(0, 4, 10, 10)
+        #Перебираем все предсказания нейросети
         for i in range(len(filenames)):
+            #Считываем изображение
             image = wx.Image(filenames[i], wx.BITMAP_TYPE_ANY)
+            #Обрезаем изображение
             image = image.Scale(200, h)
+            #Переводим изображение в битовый формат
             bitmap = wx.Bitmap(image)
+            #Выводим изображение в виде кнопки
             bitmapButton = wx.BitmapButton(panel, id=wx.ID_ANY, bitmap=bitmap, pos=(10, 10))
-
-            # Add the image name as label above the bitmapButton
+            #Выводим ФИО к картинке
             label = wx.StaticText(panel, id=wx.ID_ANY, label=name[i])
-
-            # Create sizer for each row
+            #Формируем линию
             vbox = wx.BoxSizer(wx.VERTICAL)
-
-            # Add label above bitmapButton to vbox
+            #Выводим кнопки
             vbox.Add(label, 0, wx.ALIGN_CENTER, 10)
             vbox.Add(bitmapButton, 0, wx.ALIGN_CENTER, 10)
-
-            # Add vbox to grid sizer
+            #Меняем размеры в зависимости от ширины окна
             gridSizer.Add(vbox, 0, wx.EXPAND, 10)
 
-
-        bSizer.Add(gridSizer, 1, wx.ALL, 10)  # Add grid sizer to the main sizer
+        #Отрисовываем все элементы
+        bSizer.Add(gridSizer, 1, wx.ALL, 10)
         panel.Layout()
         panel.Show()
         panel.SetSizer(bSizer)
         self.Centre(wx.BOTH)
         panel.SetSizerAndFit(bSizer)
 
+    #Создаем функции для кнопки "Подтверждаю"
     def on_identify(self, event):
-        # Append "1" to the excel file
+        #Открываем базу
         wb = openpyxl.load_workbook('filename.xlsx')
         ws = wb.active
+        #Записываем в базу ФИО, дату и время
         ws.append([result, current_date, current_time])
+        #Сохраняем базу
         wb.save('filename.xlsx')
+        #Закрываем всплывающее окно
         self.Close()
+
+    #Создаем функции для кнопки "Ошибка"
     def on_not_recognized(self, event):
+        #Закрывем всплывающее окно
         self.Close()
 
-
+#Вызываем всплывающее окно в постоянном потоке
 if __name__ == '__main__':
     app = wx.App()
     frame = MyFrame()
--- start.py
+++ start.py
@@ -5,52 +5,20 @@ import pyautogui
 import time
 import pathlib
 import os
-# class ProgressDialog(wx.Dialog):
-#     def __init__(self, parent):
-#         wx.Dialog.__init__(self, parent, title="Окно с баром загрузки", size=(300, 200))
-#
-#         panel = wx.Panel(self)
-#         # bSizer = wx.BoxSizer(wx.VERTICAL)
-#         # Создаем бар загрузки
-#         self.gauge = wx.Gauge(panel, range=10)
-#
-#         # Создаем таймер для обновления бара загрузки
-#         self.timer = wx.Timer(self)
-#         self.Bind(wx.EVT_TIMER, self.on_timer, self.timer)
-#         self.count = 0
-#
-#         # Размещаем элементы на панели
-#         sizer = wx.BoxSizer(wx.VERTICAL)
-#         sizer.Add(self.gauge, 0, wx.EXPAND | wx.ALL, 20)
-#         panel.SetSizer(sizer)
-#
-#         # Устанавливаем размеры окна
-#         self.SetSize(300, 200)
-#
-#         # Запускаем таймер
-#         self.timer.Start(500)
-#         panel.Layout()
-#         panel.Show()
-#         self.Centre(wx.BOTH)
-#         # panel.SetSizer(bSizer)
-#
-#
-#     def on_timer(self, event):
-#         # Обновляем значение бара загрузки
-#         self.count += 1
-#         if self.count > 10:
-#             self.count = 0
-#         self.gauge.SetValue(self.count)
-#         self.Close()
+
+#Создаем функцию, которая открывает Internet Explorer и делает скриншот области в определенной странице
 def screen():
     pyautogui.FAILSAFE = False
+    #Открыть браузер
     pyautogui.hotkey('win', 's')
     pyautogui.typewrite('Internet Explorer')
     pyautogui.press('enter')
+    #Подсветить страничку
     time.sleep(0.5)
+    #Закрыть приветственную страничку
     pyautogui.hotkey('ctrl', 'w')
 
-    # Активация окна Internet Explorer
+    # Активация нужной вкладки
     window_title = "Орландо Блум (Orlando Bloom): фильмы, биография, семья, фильмография — Кинопоиск"
     pyautogui.getWindowsWithTitle(window_title)[0].activate()
     # Определение координат и размеров области для обрезки
@@ -62,45 +30,55 @@ def screen():
     screenshot = pyautogui.screenshot()
     cropped_screenshot = screenshot.crop((left, top, width, height))
     # Сохранение обрезанного скриншота в файл
-    cropped_screenshot.save(os.path.abspath("Screen/screenshot.png"))
+    cropped_screenshot.save(os.path.abspath("Screen/screenshot.jpg"))
     # Свернуть окно Internet Explorer
     pyautogui.getWindowsWithTitle(window_title)[0].minimize()
 
+#Создадим всплывающее окно
 class MyFrame(wx.Frame):
+    #Инициирование всплывающего окна
     def __init__(self):
         wx.Frame.__init__(self, None, id=wx.ID_ANY, title="Resize This Window", size=wx.Size(520, 500))
         panel = wx.Panel(self, -1)
         bSizer = wx.BoxSizer(wx.VERTICAL)  # Changed to vertical sizer
+        #Цвет фона
         self.SetBackgroundColour("white")
 
-        # Add "Identify" button
+        # Создаем кноку "Распознать сотрудника"
         self.identifybutton = wx.Button(panel, label="Распознать сотрудника", size=(500, 100))
         self.identifybutton.Bind(wx.EVT_BUTTON, self.on_identify)
+        # Задаем координаты кнопки
         bSizer.Add(self.identifybutton, 0, wx.TOP, 50)
+        #Определяем и применяем шрифт
         font = wx.Font(34, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL)
         self.identifybutton.SetFont(font)
 
-        # Add "Not Recognized" button
+        # Создаем кнопку "Завершить"
         self.notrecognizedbutton = wx.Button(panel, label="Завершить", size=(500, 100))
         self.notrecognizedbutton.Bind(wx.EVT_BUTTON, self.on_not_recognized)
+        #Заадаем координаты кнопки
         bSizer.Add(self.notrecognizedbutton, 0, wx.TOP, 50)
+        #Применяем шрифт
         self.notrecognizedbutton.SetFont(font)
-
+        #Выводим панель и все элементы
         panel.Layout()
         panel.Show()
         panel.SetSizer(bSizer)
         self.Centre(wx.BOTH)
-        # panel.SetSizerAndFit(bSizer)
 
-
+    #Определяем функции кнопки распознавания
     def on_identify(self, event):
+        #Делаем скриншот
         screen()
+        #Вызываем форму верификации
         subprocess.call(os.path.abspath("start_gallery_deepface.bat"), shell=True)
 
+    #Определяем функции кнопки ошибки
     def on_not_recognized(self, event):
+        #Закрываем кнопку
         self.Close()
 
-
+#Вызываем всплывающее окно в постоянном потоке
 if __name__ == '__main__':
     app = wx.App()
     frame = MyFrame()
